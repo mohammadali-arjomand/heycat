95cfbbaf09045a010f37261482274205d23f2aec
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-07-15 13:37:21 +0330

    improve time worker

--- workers/time.php
+++ workers/time.php
@@ -1,8 +1,15 @@
 #!/usr/bin/php
 <?php
 
-echo "Now is ";
-if (stripos($argv[1], "date")) {
-    echo date("Y/m/d ");
+if (stripos($argv[1], "date") && stripos($argv[1], "time")) {
+    echo "Now is ";
+    echo date("Y/m/d h:i:s") . "\n";
 }
-echo date("h:i:s") . "\n";
+else if (stripos($argv[1], "date")) {
+    echo "Today is ";
+    echo date("Y/m/d") . "\n";
+}
+else {
+    echo "Time is ";
+    echo date("h:i:s") . "\n";
+}
